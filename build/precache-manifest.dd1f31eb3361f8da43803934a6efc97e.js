18a7758bcd2ef9b2e98ad8cc4b01d78e8c058f58
nisaraliyawari <69773572+nisaraliyawari@users.noreply.github.com>
2021-06-01 15:12:12 +1000

    build version

---
+++ build/precache-manifest.dd1f31eb3361f8da43803934a6efc97e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "430a8f1a33a9a9e79502a306004f5cf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8df30238c1910eb99c03",
+    "url": "/static/css/2.1add2298.chunk.css"
+  },
+  {
+    "revision": "3d12ac0c8eee8fde579a",
+    "url": "/static/css/main.a714ed47.chunk.css"
+  },
+  {
+    "revision": "8df30238c1910eb99c03",
+    "url": "/static/js/2.2cda12fa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2cda12fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d12ac0c8eee8fde579a",
+    "url": "/static/js/main.7560118b.chunk.js"
+  },
+  {
+    "revision": "aeed399862a4833e1ba1",
+    "url": "/static/js/runtime-main.3afedb2a.js"
+  }
+]);
